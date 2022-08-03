46b39de174a53006f34c88401f8b420cda3ae288
Li Wing <liwramos1@gmail.com>
2022-08-03 16:25:09 -0300

    update graphs

--- main.m
+++ main.m
@@ -1,43 +1,57 @@
 NUMBER_OF_SAMPLES = 20;
+NUMBER_OF_MEAN = 10;
 
 sample = zeros(10, NUMBER_OF_SAMPLES);
 
 % SIMULATE RECHARGE RATE
-%{
-for k = 1:10
-  
+
+n = [1;5;12];
+
+for k = 1:3
+
   for i = 1:NUMBER_OF_SAMPLES
-    
-    for j = 1:10
-      sample(k,i) += (simulation(100, 0.1*i, 1000, 0.01*k));
+
+    for j = 1:NUMBER_OF_MEAN
+      sample(k,i) += (simulation(50, 0.1*i, 1000000, 0.01*n(k)));
     endfor
-    
-    sample(k,i) /= j;
-    
+
+    sample(k,i) /= NUMBER_OF_MEAN;
+
   endfor
-  
+
 endfor
-%}
-
-% SIMULATE RECHARGE RATE
-
-for k = 2:2:6
-  
-  for i = 1:NUMBER_OF_SAMPLES
-    
-    for j = 1:10
-      sample(k,i) += (simulation(10^k, 0.1*i, 1000, 0.1));
-    endfor
-    
-    sample(k,i) /= j;
-    
-  endfor
-  
-endfor
-
-
 
 y = (0.1:0.1:NUMBER_OF_SAMPLES/10);
-plot(y,sample(2,:),'ro-', y,sample(4,:),'go-', y,sample(6,:),'bo-', y,sample(8,:),'rs-', y,sample(10,:),'gs-' );
-h = legend("2%", "4%", "6%", "8%", "10%");
+plot(y,sample(1,:),'ro-', y,sample(2,:),'go-', y,sample(3,:),'bo-');
+h = legend("1", "5", "12");
 legend(h, "location", "southeast");
+xlabel("arrivalRate");
+ylabel("succesRate");
+title("Recharge Rate - 50 Devices");
+
+% SIMULATE NUMBER DEVICES
+%{
+n = [50;100;200];
+
+for k = 1:3
+
+  for i = 1:NUMBER_OF_SAMPLES
+
+    for j = 1:NUMBER_OF_MEAN
+      sample(k,i) += (simulation(n(k), 0.1*i, 1000, 0.1));
+    endfor
+
+    sample(k,i) /= NUMBER_OF_MEAN;
+
+  endfor
+
+endfor
+
+y = (0.1:0.1:NUMBER_OF_SAMPLES/10);
+plot(y,sample(1,:),'ro-', y,sample(2,:),'go-', y,sample(3,:),'bo-');
+h = legend("50", "100", "200");
+legend(h, "location", "southeast");
+xlabel("arrivalRate");
+ylabel("succesRate");
+title("Number of Devies")
+%}
